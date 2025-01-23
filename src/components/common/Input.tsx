5c5508644a1c6c7c37c8c447fc600ce5b76eaa98
yeonilil <gwagseoyeon@gmail.com>
2025-01-23 21:57:06 +0900

    TKT-28 feat: input 컴포넌트 구현

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,33 @@
+interface InputProps {
+  element: 'input' | 'textarea';
+  label?: string;
+  size: 'sm' | 'lg';
+  placeholder?: string;
+  required?: boolean;
+  maxLength?: number;
+}
+
+export default function Input({element, label, size, placeholder, required, maxLength}: InputProps) {
+  return (
+    <>
+      <label className="text-sm font-semibold ml-[5px]">{label}</label>
+      {element === 'input' ? (
+        <input
+          placeholder={placeholder}
+          maxLength={maxLength}
+          required={required}
+          className={` ${size === 'sm' ? 'mt-[10px]' : ''} w-full px-4 py-[9px] border border-gray-2 rounded-md focus:outline-none focus:border-main `}
+        />
+      ) : (
+        <textarea
+          placeholder={placeholder}
+          required={required}
+          maxLength={maxLength}
+          className={
+            ' resize-none h-[110px] mt-[8px] px-[20px] py-[12px] text-black-0 border border-gray-5 rounded-[10px] focus:outline-none focus:border-primary-1 focus:border-[2px]'
+          }
+        />
+      )}
+    </>
+  );
+}
--- src/pages/manager/ManagerHome.tsx
+++ src/pages/manager/ManagerHome.tsx
@@ -1,13 +1,19 @@
 // 담당자 - 티켓 관리 대시보드 (전체)
 
+import Input from '../../components/common/Input';
 import TopMenu from '../../components/common/TopMenu';
 
 export default function ManagerHome() {
   return (
     <div className="top-container">
       <div className="flex flex-col max-w-1200">
-        <TopMenu boldBlackText="Dashboard" boldGrayText="티켓 관리 대시보드" rightText="나의 티켓 관리 바로가기" linkTo="/manager/tickets" />
-
+        <TopMenu
+          boldBlackText="Dashboard"
+          boldGrayText="티켓 관리 대시보드"
+          rightText="나의 티켓 관리 바로가기"
+          linkTo="/manager/tickets"
+        />
+        <Input element="input" size="sm" placeholder="현재 비밀번호를 입력해주세요" />
       </div>
     </div>
   );
