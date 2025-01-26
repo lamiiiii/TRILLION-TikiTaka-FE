0044d22101cb66da51129fd0f799511c25f07caf
김낙도 <tpcmsk0920@gmail.com>
2025-01-26 16:15:06 +0900

    TKT-24 feat : 티켓 상태별 카테고리 필터 구현

---
+++ src/interfaces/ticket.ts
@@ -0,0 +1,1 @@
+export type TicketViewType = '전체' | '대기중' | '진행중' | '검토 요청' | '완료' | '긴급';
--- src/pages/manager/ManagerHome.tsx
+++ src/pages/manager/ManagerHome.tsx
@@ -1,14 +1,11 @@
 // 담당자 - 티켓 관리 대시보드 (전체)
 
-import TopMenu from '../../components/common/TopMenu';
+import ManagerHomeContainer from '../../components/manager/home/ManagerHomeContainer';
 
 export default function ManagerHome() {
   return (
     <div className="top-container">
-      <div className="flex flex-col max-w-1200">
-        <TopMenu boldBlackText="Dashboard" boldGrayText="티켓 관리 대시보드" rightText="나의 티켓 관리 바로가기" linkTo="/manager/tickets" />
-
-      </div>
+      <ManagerHomeContainer />
     </div>
   );
 }
