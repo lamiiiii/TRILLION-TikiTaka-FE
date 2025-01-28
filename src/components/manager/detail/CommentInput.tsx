3f14a4c5c8f7a0ce30fd549b6256837f70a54543
yeonilil <gwagseoyeon@gmail.com>
2025-01-28 13:20:16 +0900

    TKT-39 feat: 티켓 생성 첨부파일 첨부 구현

--- src/components/manager/detail/CommentInput.tsx
+++ src/components/manager/detail/CommentInput.tsx
@@ -1,20 +1,39 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import DropDown from '../../common/Dropdown';
 import Profile from '../../common/Profile';
 
 export default function CommentInput() {
   const [selectedManager, setSelectedManager] = useState('@Alex');
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState('');
+
   const handleManagerSelect = (selectedOption: string) => {
     setSelectedManager(selectedOption);
+  };
+
+  const handleFileButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setFileName(file.name);
+    }
   };
 
   return (
     <div className="mt-5">
       <div className="flex gap-3">
         <DropDown label="@mention" options={['@Alex', '@Jojo', '@Yeon']} defaultSelected={selectedManager} onSelect={handleManagerSelect} />
-        <button className="rounded-md py-1 px-6 text-caption-regular border border-main hover:bg-main hover:text-white">
+        <button
+          className="rounded-md py-1 px-6 text-caption-regular border border-main hover:bg-main hover:text-white"
+          onClick={handleFileButtonClick}
+        >
           첨부파일 첨부
         </button>
+        {fileName && <span className="mt-2 text-caption-regular">{fileName}</span>}
+        <input type="file" accept="image/*" ref={fileInputRef} style={{display: 'none'}} onChange={handleFileChange} />
       </div>
       <div className="relative">
         <div className="flex gap-2 mt-3 mb-2">
