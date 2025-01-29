c4d1040305224b4348cc8c71859cf3b31bae8ac1
yeonilil <gwagseoyeon@gmail.com>
2025-01-29 21:47:31 +0900

    TKT-41 feat: 답변 토글 구현

--- src/components/manager/inquiry/Inquiry.tsx
+++ src/components/manager/inquiry/Inquiry.tsx
@@ -1,19 +1,55 @@
+import {useState} from 'react';
 import {InquiryData} from '../../../interfaces/interfaces';
+import {DownIcon} from '../../common/Icon';
 
 interface InquiryProps {
   data: InquiryData;
 }
 
 export default function Inquiry({data}: InquiryProps) {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+
+  const toggleAnswerVisibility = () => {
+    setIsAnswerVisible((prev) => !prev);
+  };
+
   return (
-    <div className="flex w-full p-5 rounded bg-white border border-gray-2 text-subtitle-regular">
-      <p className="mr-[59px]">{data.type}</p>
-      <div>
-        <p>{data.title}</p>
-        <p className="w-[450px] text-gray-6 text-body-regular mt-1 mr-[120px]">{data.content}</p>
-      </div>
-      <p className="mr-[55px]">{data.date}</p>
-      <p>{data.status}</p>
+    <div className="flex flex-col gap-2">
+      <section className="flex w-full p-5 rounded bg-white border border-gray-2 text-subtitle-regular">
+        <p className="mr-[59px]">{data.type}</p>
+        <div>
+          <p>{data.title}</p>
+          <p className="w-[450px] text-gray-6 text-body-regular mt-1 mr-[120px]">{data.content}</p>
+        </div>
+        <p className="mr-[55px]">{data.date}</p>
+        <div className="relative flex gap-2">
+          <p>{data.status}</p>
+
+          {data.status === '답변 완료' && (
+            <div
+              className="absolute top-[-4px] left-16"
+              style={{
+                color: 'black',
+                transform: isAnswerVisible ? 'rotate(180deg)' : 'rotate(0deg)',
+                transition: 'transform 0.3s ease',
+              }}
+              onClick={toggleAnswerVisibility}
+            >
+              <DownIcon />
+            </div>
+          )}
+        </div>
+      </section>
+
+      {isAnswerVisible && data.answer && (
+        <section className="flex w-full p-5 rounded bg-gray-1 border border-gray-2 text-subtitle-regular">
+          <p className="mr-[30px]">문의 답변</p>
+          <div>
+            <p> Re: {data.title}</p>
+            <p className="w-[600px] text-gray-6 text-body-regular mt-1 mr-[120px]">{data.answer?.content}</p>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -49,4 +49,7 @@ export interface InquiryData {
   content: string;
   date: string;
   status: string;
+  answer?: {
+    content: string;
+  };
 }
