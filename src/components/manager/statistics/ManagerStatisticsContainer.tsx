15633da1863a36443c25a9bf5264a68ff96b1c02
yeonilil <gwagseoyeon@gmail.com>
2025-02-04 00:29:27 +0900

    TKT-54 feat: 담당자 시간 통계, 월별 컨테이너 구현

--- src/components/manager/statistics/ManagerStatisticsContainer.tsx
+++ src/components/manager/statistics/ManagerStatisticsContainer.tsx
@@ -1,21 +1,41 @@
+import {useState} from 'react';
 import CategoryTicketStatus from '../../common/statistics/CategoryTicketStatus';
 import TopMenu from '../../common/TopMenu';
-import ManagerTicketStatus from './ManagerTicketStatus';
-import TodayTicketStatus from './TodayTicketStatus';
+import ManagerTicketStatus from './daily/ManagerTicketStatus';
+import StatisticsFilter from './StatisticsFilter';
+
+import TodayTicketStatus from './daily/TodayTicketStatus';
+import ManagerTicketPeriodStatus from './monthly/ManagerTicketPeriodStatus';
 
 export default function ManagerStatisticsContainer() {
+  const [selectedFilter, setSelectedFilter] = useState('일별');
+
   return (
-    <div className="flex flex-col max-w-1200 pt-[30px] px-[46px]">
+    <div className="flex flex-col max-w-1200 px-[46px]">
       <TopMenu boldBlackText="통계 관리" rightText="티켓 관리 대시보드 바로가기" linkTo="/manager" />
-      <section className="flex bg-gray-18 p-6 pb-[38px] mt-3 mb-[100px]">
-        <div className="flex flex-col">
-          <div className="w-full grid grid-cols-2">
-            <TodayTicketStatus />
-            <ManagerTicketStatus />
+      <StatisticsFilter onFilterChange={setSelectedFilter} />
+
+      {selectedFilter === '일별' ? (
+        <section className="flex bg-gray-18 p-6 pb-[38px] mt-3 mb-[100px]">
+          <div className="flex flex-col">
+            <div className="w-full grid grid-cols-2">
+              <TodayTicketStatus />
+              <ManagerTicketStatus />
+            </div>
+            <CategoryTicketStatus />
           </div>
-          <CategoryTicketStatus />
-        </div>
-      </section>
+        </section>
+      ) : (
+        <section className="flex bg-gray-18 p-6 pb-[38px] mt-3 mb-[100px]">
+          <div className="flex flex-col">
+            <div className="w-full grid grid-cols-2">
+              <ManagerTicketPeriodStatus />
+              <TodayTicketStatus />
+            </div>
+            <CategoryTicketStatus />
+          </div>
+        </section>
+      )}
     </div>
   );
 }
