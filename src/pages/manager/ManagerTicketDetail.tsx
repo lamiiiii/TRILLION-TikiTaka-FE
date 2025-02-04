9fe10a02f12663f3b0614374487589b3299181ae
yeonilil <gwagseoyeon@gmail.com>
2025-02-04 09:30:18 +0900

    TKT-71 fix: 티켓 세부 페이지 깨지는 현상 수정

--- src/pages/manager/ManagerTicketDetail.tsx
+++ src/pages/manager/ManagerTicketDetail.tsx
@@ -3,7 +3,7 @@ import ManagerDetailContainer from '../../components/manager/detail/ManagerDetai
 export default function ManagerTicketDetail() {
   return (
     <div className="top-container">
-      <div className="flex flex-col max-w-1200">
+      <div className="flex flex-col">
         <ManagerDetailContainer />
       </div>
     </div>
