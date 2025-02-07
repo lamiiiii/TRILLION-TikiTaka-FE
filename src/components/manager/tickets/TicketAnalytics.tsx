9ab2ee8cdaf91a6c739b263213af9edf883d9307
yeonilil <gwagseoyeon@gmail.com>
2025-02-07 14:05:33 +0900

    TKT-103 fix: 요일 통계 데이터 없을 시 예외 처리

--- src/components/manager/tickets/TicketAnalytics.tsx
+++ src/components/manager/tickets/TicketAnalytics.tsx
@@ -24,30 +24,34 @@ export default function TicketAnalytics() {
     : [];
 
   return (
-    <div className="flex items-center gap-6 w-full bg-gray-18 p-5">
-      <section>
-        <p className="pl-2 text-caption-regular text-gray-14">단위: 건</p>
-        <BarChart width={240} height={120} data={chartData}>
-          <XAxis dataKey="name" tick={{fontSize: 14}} axisLine={false} tickLine={false} />
-          <Bar dataKey="ticket" fill="#F6D47A" radius={100}>
-            <LabelList dataKey="ticket" position="center" fill="#FFFFFF" height={40} fontSize={10} />
-          </Bar>
-        </BarChart>
-      </section>
+    <>
+      {weeklyData && (
+        <div className="flex items-center gap-6 w-full bg-gray-18 p-5">
+          <section>
+            <p className="pl-2 text-caption-regular text-gray-14">단위: 건</p>
+            <BarChart width={240} height={120} data={chartData}>
+              <XAxis dataKey="name" tick={{fontSize: 14}} axisLine={false} tickLine={false} />
+              <Bar dataKey="ticket" fill="#F6D47A" radius={100}>
+                <LabelList dataKey="ticket" position="center" fill="#FFFFFF" height={40} fontSize={10} />
+              </Bar>
+            </BarChart>
+          </section>
 
-      <section className="grid grid-cols-2 text-subtitle">
-        <div className="space-y-4">
-          <div>금일 티켓 처리 건수</div>
-          <div>금일 긴급 티켓 건수</div>
-          <div>금주 티켓 처리 건수</div>
-        </div>
+          <section className="grid grid-cols-2 text-subtitle">
+            <div className="space-y-4">
+              <div>금일 티켓 처리 건수</div>
+              <div>금일 긴급 티켓 건수</div>
+              <div>금주 티켓 처리 건수</div>
+            </div>
 
-        <div className="text-right text-main2-3 space-y-4">
-          <div>{weeklyData && weeklyData?.dayTickets}건</div>
-          <div>{weeklyData && weeklyData?.dayUrgentTickets}건</div>
-          <div>{weeklyData && weeklyData?.weekTickets}건</div>
+            <div className="text-right text-main2-3 space-y-4">
+              <div>{weeklyData?.dayTickets}건</div>
+              <div>{weeklyData && weeklyData?.dayUrgentTickets}건</div>
+              <div>{weeklyData && weeklyData?.weekTickets}건</div>
+            </div>
+          </section>
         </div>
-      </section>
-    </div>
+      )}
+    </>
   );
 }
