71395664f3b399d281009cb5267b75faaf642bef
yeonilil <gwagseoyeon@gmail.com>
2025-02-07 21:29:16 +0900

    TKT-104 fix: 프로필 박스 기본 프로필 사진 추가 및 문의 사이드메뉴 제거

--- src/components/common/UserBox.tsx
+++ src/components/common/UserBox.tsx
@@ -3,21 +3,21 @@ import {useUserStore} from '../../store/store';
 import {getUserInfo} from '../../api/service/users';
 
 export default function UserBox() {
-  const {userId, setUserId, role, setRole, setUserName} = useUserStore();
+  const {userId, setUserId, setRole, setUserName} = useUserStore();
   const [userInfo, setUserInfo] = useState<UserDetailResponse | null>(null);
 
-  const getRoleColor = () => {
-    switch (role) {
-      case 'MANAGER':
-        return '#2C2C2C';
-      case 'USER':
-        return '#F6D47A';
-      case 'ADMIN':
-        return '#16407B';
-      default:
-        return '#FFFFFF';
-    }
-  };
+  // const getRoleColor = () => {
+  //   switch (role) {
+  //     case 'MANAGER':
+  //       return '#2C2C2C';
+  //     case 'USER':
+  //       return '#F6D47A';
+  //     case 'ADMIN':
+  //       return '#16407B';
+  //     default:
+  //       return '#FFFFFF';
+  //   }
+  // };
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -44,7 +44,7 @@ export default function UserBox() {
         {userInfo.profileImageUrl ? (
           <img src={userInfo.profileImageUrl} alt="Profile" className="w-full h-full object-cover rounded-full" />
         ) : (
-          <div className="w-full h-full rounded-full" style={{backgroundColor: getRoleColor()}}></div>
+          <img src="/assets/profile.png" alt="Profile" className="w-full h-full  rounded-full" />
         )}
       </div>
       <div className="text-main text-subtitle">{userInfo.username}</div>
