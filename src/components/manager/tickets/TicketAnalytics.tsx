01ab8a04025ee617e0add21cef1427d76bff1250
yeonilil <gwagseoyeon@gmail.com>
2025-02-11 13:28:17 +0900

    TKT-156 fix: 담당자 통계 잘리는 부분 수정

--- src/components/manager/tickets/TicketAnalytics.tsx
+++ src/components/manager/tickets/TicketAnalytics.tsx
@@ -30,7 +30,7 @@ export default function TicketAnalytics() {
           <section>
             <p className="pl-2 text-caption-regular text-gray-14">단위: 건</p>
             <BarChart width={240} height={120} data={chartData}>
-              <XAxis dataKey="name" tick={{fontSize: 14}} axisLine={false} tickLine={false} />
+              <XAxis dataKey="name" tick={{fontSize: 12}} axisLine={false} tickLine={false} />
               <Bar dataKey="ticket" fill="#F6D47A" radius={100}>
                 <LabelList dataKey="ticket" position="center" fill="#FFFFFF" height={40} fontSize={10} />
               </Bar>
