fe6ada7a749fde1a5dd5d148181a2cdf61062c25
lamiiiii <guelee0213@naver.com>
2025-02-19 18:51:52 +0900

    빈커밋

--- src/components/common/siginin/InitialLayout.tsx
+++ src/components/common/siginin/InitialLayout.tsx
@@ -8,7 +8,6 @@ export default function InitialLayout({children}: InitialLayoutProps) {
   return (
     <div className="relative flex w-full min-h-screen">
       <InitialTopBar />
-      {/* 우측 */}
       {children}
     </div>
   );
